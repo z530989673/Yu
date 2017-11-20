fac032986401f307d45aa971529527b26275c118
owlies.zihan@gmail.com <owlies.zihan@gmail.com>
2017-11-20 07:49:04 -0800

    remove generated files and load map

--- src/LayaSample.ts
+++ src/LayaSample.ts
@@ -1,8 +1,25 @@
 // 程序入口
+import Sprite = Laya.Sprite;
+import Stage = Laya.Stage;
+import Texture = Laya.Texture;
+import Browser = Laya.Browser;
+import Handler = Laya.Handler;
+import WebGL = Laya.WebGL;
+
 class GameMain{
     constructor()
     {
-        Laya.init(600,400);
+        Laya.init(Browser.clientWidth, Browser.clientHeight, WebGL);
+
+        Laya.stage.alignV = Stage.ALIGN_MIDDLE;
+        Laya.stage.alignH = Stage.ALIGN_CENTER;
+
+        Laya.stage.scaleMode = "showall";
+        Laya.stage.bgColor = "#232628";
+        
+        var ape = new Sprite();
+		Laya.stage.addChild(ape);
+		ape.loadImage("../laya/assets/Map/map.jpg");
     }
 }
 new GameMain();
